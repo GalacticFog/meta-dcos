2c8b72ade3a8be29e0986ab9334b0275a28c93f2
Christopher Baker <cgbaker@gmail.com>
2016-07-19 00:41:57 -0400

    removed old demo script, not useful anymore

--- bind-prod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -x 
-
-./meta-dcos --user=root --password=letmein --meta-host=meta.galacticfog.com --meta-port=80 --environment=galacticfog/DemoWorkspace/Prod --provider=GF_Cluster
-
